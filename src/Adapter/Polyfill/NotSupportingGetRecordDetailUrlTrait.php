f21f934cc8538cd91ff21b2b57c0345b09b9923f
Fish <chao.yen.po@gmail.com>
2020-01-22 17:16:15 +0800

    fix bug

--- src/Adapter/Polyfill/NotSupportingGetRecordDetailUrlTrait.php
+++ src/Adapter/Polyfill/NotSupportingGetRecordDetailUrlTrait.php
@@ -4,7 +4,7 @@ namespace Gamesmkt\Fishpond\Adapter\Polyfill;
 
 use Gamesmkt\Fishpond\Exception\NotSupportingException;
 
-trait NotSupportingGetGameResultUrlTrait
+trait NotSupportingGetRecordDetailUrlTrait
 {
     /**
      * 取得遊戲結果的網址。
@@ -15,10 +15,10 @@ trait NotSupportingGetGameResultUrlTrait
      *
      * @throws \Gamesmkt\Fishpond\Exception\NotSupportingException
      */
-    public function getGaemResultlUrl(RecordInterface $record, GameInterface $game, Config $config)
+    public function getRecordDetailUrl(RecordInterface $record, GameInterface $game, Config $config)
     {
         throw new NotSupportingException(
-            get_class($this) . ' does not support get game result url.'
+            get_class($this) . ' does not support get record detail url.'
         );
     }
 }
